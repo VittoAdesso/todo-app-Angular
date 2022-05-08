141a614d8dfd0c08b82b5b7363d77fd00e689c2f
Vittorio Adesso <adessovittorio8@gmail.com>
2022-05-08 23:32:15 +0200

    new features

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { DataService } from '../shared/data.service';
 import { Todo } from '../shared/todo.model';
+import { DataService } from '../shared/data.service';
+import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTodoDialogComponent } from '../edit-todo-dialog/edit-todo-dialog.component';
 
 @Component({
   selector: 'app-todos',
@@ -10,18 +12,43 @@ import { Todo } from '../shared/todo.model';
 })
 export class TodosComponent implements OnInit {
 
-  todos!: Todo[];
-  
-  constructor(private dataService: DataService) { }
+  todos!: Todo[]
 
-    ngOnInit() : void{
-      this.todos = this.dataService.getAllTodos();
-    }
-    
-    onFormSubmit(form: NgForm) {
-      console.log(form)
+  constructor(private dataService: DataService, private dialog: MatDialog) { }
 
-      this.dataService.addTodo( new Todo( form.value.text) ) 
-    }
+  ngOnInit(): void {
+    this.todos = this.dataService.getAllTodos()
+  }
+
+  onFormSubmit(form: NgForm) {   
+
+    this.dataService.addTodo(new Todo(form.value.text))
+
+    form.reset()
+  }
+
+  toggleCompleted(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  editTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+
+    let dialogRef = this.dialog.open(EditTodoDialogComponent, {
+      width: '700px',
+      data: todo
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.dataService.updateTodo(index, result)
+      }
+    })
+  }
+
+  deleteTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+    this.dataService.deleteTodo(index)
+  }
   
 }
