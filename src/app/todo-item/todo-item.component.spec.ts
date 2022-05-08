7714d152b6bf00b14ebd7035f7924c5d3f3832d1
Vittorio Adesso <adessovittorio8@gmail.com>
2022-05-08 23:32:45 +0200

    added new functions & styles & mui

---
+++ src/app/todo-item/todo-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TodoItemComponent } from './todo-item.component';
+import { Todo } from '../shared/todo.model';
+
+describe('TodoItemComponent', () => {
+  let component: TodoItemComponent;
+  let fixture: ComponentFixture<TodoItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TodoItemComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
